2a619fb2cc3c4f4fae2c6853f4dcb14726b278da
EC2 Default User <ec2-user@ip-172-31-4-223.ec2.internal>
2021-04-12 16:34:48 +0000

    Change todo status & add new todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,10 +5,20 @@
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
 function TodoItem(  ) {
+function TodoItem( { item } ) {
   return (
     <label className="panel-block">
+    	<input type="checkbox"/>
+    	{item.text}
+      <input
+        type="checkbox"
+        checked={item.done}
+        onChange={handleChange}
+      />
+      <span className={item.done ? 'has-text-grey-light' : ''}>
+      {item.text}
+      </span>
     </label>
   );
 }
-
 export default TodoItem;
